b1d190f6bb6e3a794c1abc1988180927cb1119e1
Karsten Beck <s3912792@student.rmit.edu.au>
2023-09-03 04:30:01 +1000

    /user GET all, /user POST new user, /user/:id working

--- models/user.js
+++ models/user.js
@@ -1,26 +1,33 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
-/*                                                  User Model                                                        */
+/*---------------------------------------              User Model              ---------------------------------------*/
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String
-    },
-    accessLevel: {
-        type: Number
-    },
-    email: {
-        type: String
-    },
-    password: {
-        type: String
-    }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String,
+  },
+  accessLevel: {
+    type: Number,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,58 +1,77 @@
 // User Routes
 
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
 
 // GET - Get all users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const findResult = await User.find({})
-        res.render('user/index', {users: findResult})
+        const result = await User.find({});
+        res.render("user/index", {users: result});
     } catch (err) {
-        res.redirect('/')
+        res.redirect("/");
     }
-})
+});
 
 // GET - Show Form to input new user
-router.get('/new', (req, res) => {
-    res.render('user/newUser', {user: new User()})
-})
+router.get("/new", (req, res) => {
+    res.render("user/newUser", {user: new User()});
+});
 
 // GET - Get specific user by id
-router.get('/:id', async (req, res) => {
-    let userId = req.params.id
-    
-    try{
-        const foundUser = await User.findById(userId).exec();
-        console.log('User Found')
-        res.send(foundUser);
-    } catch (err){
-        res.redirect('/')
-        console.log("Entry not found.")
-    }
-})
+router.get("/:id", async (req, res) => {
+    let userId = req.params.id;
 
-// PUT - Update user with id
-router.put('/', async (req, res) => {
-    let userId = req.params.id
-    
-    try{
+    try {
         const foundUser = await User.findById(userId).exec();
-        console.log('User Found')
+        console.log("User Found");
         res.send(foundUser);
-    } catch (err){
-        res.redirect('/')
-        console.log("Entry not found.")
+    } catch (err) {
+        res.redirect("/");
+        console.log("Entry not found.");
     }
-})
+});
 
-router.post('/', async (req, res) => {
+
+// POST - Receive Form Data from GET('/new')
+router.post("/", async (req, res) => {
+
+    // Setup new User object with data from request body.
+
+    let passwordHash;
+    const password = req.body.password;
 
     // Hash the password
-    let passwordHash = generatePasswordHash(req.body.password)
-    console.log(passwordHash)
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+            if (err) {
+                return console.log('Cannot encrypt');
+            }
+
+            passwordHash = hash;
+            console.log(hash);
+
+            bcrypt.compare(password, passwordHash,
+                async function (err, isMatch) {
+
+                    if (isMatch) {
+                        console.log('Encrypted password is: ' + password);
+                        console.log('Decrypted password is: ' + passwordHash);
+                    }
+
+                    if (!isMatch) {
+                        console.log(passwordHash + 'is not encryption of ' + password)
+                    }
+                })
+        })
+    })
+
+
+    /*const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(req.body.password, salt);*/
 
     // Create new User object with hashed password
     const user = new User({
@@ -60,22 +79,45 @@ router.post('/', async (req, res) => {
         lastName: req.body.lastName,
         email: req.body.email,
         accessLevel: req.body.accessLevel,
-        password: passwordHash
-    })
+        password: passwordHash,
+    });
 
     // Save the new user to the database
     try {
-        const addUser = await user.save();
-        console.log('New user crested.')
-    } catch (err) {
+        await user.save();
+        console.log("New user crested.");
         res.redirect('/api/user');
-        console.log(err, 'No user created.')
-    }
-})
 
+        /* Because the User object defines the email to be unique, mongoose will check for this property and throw an
+        exception, if the entered email already is in the database. This will get caught here and the user returned to
+        the New User dialog with an error message. */
+    } catch (err) {
+        res.render("user/newUser", {
+            user: user,
+            errorMessage: "This email already exists in our database",
+        });
+        console.log("User already exists.");
+        console.log("No user created: " + err);
+    }
+
+});
+
+// PUT - Update user with id
+router.put("/", async (req, res) => {
+    let userId = req.params.id;
+
+    try {
+        const foundUser = await User.findById(userId).exec();
+        console.log("User Found");
+        res.send(foundUser);
+    } catch (err) {
+        res.redirect("/");
+        console.log("Entry not found.");
+    }
+});
 
 function generatePasswordHash(password) {
-    return jwt.sign(password, process.env.TOKEN_SECRET)
+    return jwt.sign(password, process.env.TOKEN_SECRET);
 }
 
-module.exports = router
+module.exports = router;
